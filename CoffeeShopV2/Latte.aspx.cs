049945b8d0b753f85db96098cff6c10c41f6cd24
Bryce McKnight <bryce.m.mcknight@gmail.com>
2018-03-14 09:58:31 -0400

    Add files via upload

---
+++ CoffeeShopV2/Latte.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CoffeeShopV2
+{
+    public partial class Latte : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void imgbtnLatte_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("Latte2.aspx");
+        }
+    }
+}
