1fa425d3f2ebc9fbdfbc21064ed379867d76e9ab
Bryce McKnight <bryce.m.mcknight@gmail.com>
2018-03-14 08:57:13 -0400

    Add files via upload

---
+++ CoffeeShopV2/Scripts/myScript.js
@@ -0,0 +1,59 @@
+﻿$("Cappuccino.aspx").ready(function () {
+    $("#cappHeader").hide().fadeIn(2000);
+    $("#cappDescription").hide().fadeIn(2000);
+    $("#cappPrices").hide().fadeIn(2000);
+});
+
+$("Cappuccino2.aspx").ready(function () {
+    $("#cappHeader2").hide().fadeIn(2000);
+    $("#cappDescription2").hide().fadeIn(2000);
+    $("#cappShort2").hide().fadeIn(2000);
+    $("#cappTall2").hide().fadeIn(2000);
+    $("#cappGrande2").hide().fadeIn(2000);
+    $("#cappVenti2").hide().fadeIn(2000);
+});
+
+$("Espresso.aspx").ready(function () {
+    $("#espHeader").hide().fadeIn(2000);
+    $("#espDescription").hide().fadeIn(2000);
+    $("#espPrices").hide().fadeIn(2000);
+});
+
+$("Espresso2.aspx").ready(function () {
+    $("#espHeader2").hide().fadeIn(2000);
+    $("#espDescription2").hide().fadeIn(2000);
+    $("#espSingle2").hide().fadeIn(2000);
+    $("#espDoppio2").hide().fadeIn(2000);
+    $("#espTriple2").hide().fadeIn(2000);
+    $("#espQuad2").hide().fadeIn(2000);
+});
+
+$("Mocha.aspx").ready(function () {
+    $("#mochaHeader").hide().fadeIn(2000);
+    $("#mochaDescription").hide().fadeIn(2000);
+    $("#mochaPrices").hide().fadeIn(2000);
+});
+
+$("Mocha2.aspx").ready(function () {
+    $("#mochaHeader2").hide().fadeIn(2000);
+    $("#mochaDescription2").hide().fadeIn(2000);
+    $("#mochaShort2").hide().fadeIn(2000);
+    $("#mochaTall2").hide().fadeIn(2000);
+    $("#mochaGrande2").hide().fadeIn(2000);
+    $("#mochaVenti2").hide().fadeIn(2000);
+});
+
+$("Latte.aspx").ready(function () {
+    $("#latteHeader").hide().fadeIn(2000);
+    $("#latteDescription").hide().fadeIn(2000);
+    $("#lattePrices").hide().fadeIn(2000);
+});
+
+$("Latte2.aspx").ready(function () {
+    $("#latteHeader2").hide().fadeIn(2000);
+    $("#latteDescription2").hide().fadeIn(2000);
+    $("#latteShort2").hide().fadeIn(2000);
+    $("#latteTall2").hide().fadeIn(2000);
+    $("#latteGrande2").hide().fadeIn(2000);
+    $("#latteVenti2").hide().fadeIn(2000);
+});
